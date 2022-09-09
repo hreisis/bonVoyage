c57b1512322ca4a9f46a929062d4a4945b816e14
Charlene Chen <jxianing@gmail.com>
2022-09-09 09:04:02 -0700

    Gestures Part 2

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react'; 
 import { StyleSheet, Text, View, PanResponder, Alert} from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
@@ -7,17 +8,25 @@ import { favouritesReducer } from '../favourites/favouriteSlice';
 const RenderCampsite = (props) => {
     const { campsite } = props;
 
+    const view = useRef();//in order to set an animation, need a reference first
+
     const isLeftSwipe = ({ dx }) => dx < -200; //dx means delta x, how far it moves along x axis
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: ()=> true,
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                .then((endState) => 
+                    console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => { //e stands for event
             console.log('pan responder end', gestureState);
-            if (isLeftSwipe(gestureState)) {
-                Alert.alert(
+            if (isLeftSwipe(gestureState)) { //check for a valid left swipe
+                Alert.alert( // 4 arguments
                     'Add Favourite',
                     'Are you sure you wish to add' + campsite.name + 'to favourites?',
-                    [
+                    [ //an array of AlertButton objects
                         {
                             text: 'Cancel',
                             styles: 'cancel',
@@ -30,7 +39,7 @@ const RenderCampsite = (props) => {
                                         : props.markFavourite() 
                         }
                     ],
-                    {cancelable: false}
+                    {cancelable: false} // a configuration object
                 )
             }
         }
@@ -42,7 +51,8 @@ const RenderCampsite = (props) => {
             animation='fadeInDownBig'
             duration={2000}
             delay={1000}
-            {...panResponder.panHandlers} //spread and combine them into new object
+            ref={view}
+            {...panResponder.panHandlers} //spread, add another prop
         >
             <Card containerStyle={ styles.cardContainer }>
                 <Card.Image source={{ uri: baseUrl + campsite.image}}>
